cffc93546f87ddbb40b7fcdc1a47500a0934288e
Christian Chiarulli <chris.machine@pm.me>
2025-03-06 04:00:27 -0500

    update website

--- README.md
+++ README.md
@@ -1,23 +1,36 @@
-<div align="center"><p>
-    <h1>cometnotes.com ☄️</h1>
-    <a href="https://github.com/nodetec/cometnotes.com/pulse">
-      <img alt="Last commit" src="https://img.shields.io/github/last-commit/nodetec/cometnotes.com?style=for-the-badge&logo=starship&color=8bd5ca&logoColor=D9E0EE&labelColor=302D41"/>
-    </a>
-    <a href="https://github.com/nodetec/cometnotes.com/stargazers">
-      <img alt="Stars" src="https://img.shields.io/github/stars/nodetec/cometnotes.com?style=for-the-badge&logo=starship&color=c69ff5&logoColor=D9E0EE&labelColor=302D41" />
-    </a>
-    <a href="https://github.com/nodetec/cometnotes.com/issues">
-      <img alt="Issues" src="https://img.shields.io/github/issues/nodetec/cometnotes.com?style=for-the-badge&logo=bilibili&color=F5E0DC&logoColor=D9E0EE&labelColor=302D41" />
-    </a>
-    <a href="https://github.com/nodetec/cometnotes.com">
-      <img alt="Repo size" src="https://img.shields.io/github/repo-size/nodetec/cometnotes.com?color=%23DDB6F2&label=SIZE&logo=codesandbox&style=for-the-badge&logoColor=D9E0EE&labelColor=302D41" />
-    </a>
-</div>
+This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
 
-Website for [Comet Notes](https://cometnotes.com/ "Comet Notes"), a note-taking app with [Nostr](https://nostr.com/ "Nostr") integration.
+## Getting Started
 
-The website hosts the Comet Notes binaries for various platforms.
+First, run the development server:
 
-## Contribute
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+# or
+bun dev
+```
 
-If you want to contribute, then check out the issues on the [Comet Notes GitHub](https://github.com/nodetec/comet "Comet Notes GitHub").
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+
+This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
+
+## Learn More
+
+To learn more about Next.js, take a look at the following resources:
+
+- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
+- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+
+You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
+
+## Deploy on Vercel
+
+The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+
+Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
---
+++ src/app/page.tsx
@@ -0,0 +1,130 @@
+/* eslint-disable @next/next/no-img-element */
+import { ChevronRightIcon } from "@heroicons/react/20/solid";
+
+export default function Example() {
+  return (
+    <div className="relative isolate overflow-hidden h-full bg-neutral-900">
+      <svg
+        aria-hidden="true"
+        className="absolute inset-0 -z-10 size-full stroke-white/10 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
+      >
+        <defs>
+          <pattern
+            x="50%"
+            y={-1}
+            id="983e3e4c-de6d-4c3f-8d64-b9761d1534cc"
+            width={200}
+            height={200}
+            patternUnits="userSpaceOnUse"
+          >
+            <path d="M.5 200V.5H200" fill="none" />
+          </pattern>
+        </defs>
+        <svg x="50%" y={-1} className="overflow-visible fill-gray-800/20">
+          <path
+            d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
+            strokeWidth={0}
+          />
+        </svg>
+        <rect
+          fill="url(#983e3e4c-de6d-4c3f-8d64-b9761d1534cc)"
+          width="100%"
+          height="100%"
+          strokeWidth={0}
+        />
+      </svg>
+      <div
+        aria-hidden="true"
+        className="absolute top-10 left-[calc(50%-4rem)] -z-10 transform-gpu blur-3xl sm:left-[calc(50%-18rem)] lg:top-[calc(50%-30rem)] lg:left-48 xl:left-[calc(50%-24rem)]"
+      >
+        <div
+          style={{
+            clipPath:
+              "polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)",
+          }}
+          className="aspect-1108/632 w-[69.25rem] bg-linear-to-r from-[#60caf3] to-[#4a42e1] opacity-20"
+        />
+      </div>
+      <div className="mx-auto max-w-7xl px-6 pt-10 pb-24 sm:pb-32 lg:flex lg:px-8 lg:py-40">
+        <div className="mx-auto max-w-2xl shrink-0 lg:mx-0 lg:pt-8">
+          <div className="flex items-center gap-1">
+            <img
+              alt="Your Company"
+              src="/comet-logo.svg"
+              className="h-11 rounded-md"
+            />
+            <span className="ml-2 text-2xl font-semibold text-white">
+              Comet
+            </span>
+            <span className="ml-2 text-xs font-semibold text-neutral-400 bg-neutral-800 p-1 border rounded-md border-neutral-700">
+              Alpha
+            </span>
+          </div>
+          <div className="mt-24 sm:mt-32 lg:mt-16">
+            <a href="#" className="inline-flex space-x-6">
+              <span className="rounded-full bg-indigo-500/10 px-3 py-1 text-sm/6 font-semibold text-indigo-400 ring-1 ring-indigo-500/20 ring-inset">
+                What&apos;s new
+              </span>
+              <span className="inline-flex items-center space-x-2 text-sm/6 font-medium text-gray-300">
+                <span>Just shipped v3-alpha</span>
+                <ChevronRightIcon
+                  aria-hidden="true"
+                  className="size-5 text-gray-500"
+                />
+              </span>
+            </a>
+          </div>
+          <h1 className="mt-10 text-5xl font-semibold tracking-tight text-pretty text-white sm:text-7xl">
+            Capture locally transmit globally.
+          </h1>
+          <p className="mt-8 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">
+            Comet is a desktop note-taking app that combines private local
+            storage with the power to publish directly to the Nostr network.
+            Organize with notebooks and tags, write in markdown, and share with
+            the world when you&apos;re ready.
+          </p>
+          <div className="mt-10">
+            <h2 className="text-2xl font-semibold text-white">Install</h2>
+            <div className="mt-6 grid grid-cols-2 sm:grid-cols-4 gap-3">
+              <a
+                href="https://github.com/nodetec/comet/releases/download/v0.3.0-alpha/Comet-Alpha.dmg"
+                className="text-center rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
+              >
+                macOS ARM
+              </a>
+              {/* <a
+                href="#"
+                className="text-center rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
+              >
+                macOS Intel
+              </a>
+              <a
+                href="#"
+                className="text-center rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
+              >
+                Linux
+              </a>
+              <a
+                href="#"
+                className="text-center rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
+              >
+                Windows
+              </a> */}
+            </div>
+          </div>
+        </div>
+        <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:mt-0 lg:mr-0 lg:ml-10 lg:max-w-none lg:flex-none xl:ml-32">
+          <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
+            <img
+              alt="App screenshot"
+              src="/demo.png"
+              width={3040}
+              height={1882}
+              className="w-[76rem]"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
